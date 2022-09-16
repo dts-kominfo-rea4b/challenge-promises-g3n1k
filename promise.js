4fafb3b67074429874f258b52d76fccce458d2cc
g3n1k <g3n1k@yahoo.com>
2022-09-16 15:03:24 +0700

    solusi_01

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (cari) => {
+  try {
+    // dapatkan raw value
+    const data_ixx = await promiseTheaterIXX();
+    const data_gvc = await promiseTheaterVGC();
+    
+    let _counter = 0;
+    data_ixx.filter(hitung);
+    data_gvc.filter(hitung);
+    function hitung(item){
+      if(item.hasil == cari) ++_counter;
+    }
+    console.log(_counter);
+
+  } catch {
+    console.log(err);
+  }
+};
+
+
+
+
 
 module.exports = {
   promiseOutput,
